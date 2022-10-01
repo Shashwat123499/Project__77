3229e5f956e0bfdde90c98968218f923e373396d
Shashwat123499 <84261321+Shashwat123499@users.noreply.github.com>
2022-10-01 11:23:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("my first console project");
+var x = "Elon";
+var y = " Musk";
+var z = x.concat(y);
+console.log(z);
+
+var a = 10;
+var b = 5;
+var c = a - b;
+console.log(c);
+var d = a + b;
+console.log (d);
+var e = a*b;
+console.log (e);
+var f = a/b;
+console.log (f);
